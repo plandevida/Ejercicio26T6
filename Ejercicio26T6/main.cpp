a717ce13ff27a049ffb1cd7d99c173ab4792761d
plandevida <plandevida@planes.com>
2013-03-28 11:34:37 +0100

    Initial Commit

---
+++ Ejercicio26T6/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ejercicio26T6
+//
+//  Created by Dani on 28/03/13.
+//  Copyright (c) 2013 Dani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
